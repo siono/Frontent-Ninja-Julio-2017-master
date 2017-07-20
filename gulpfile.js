0e5d0f29d74d91486a4a7f94a49aea4cf527629c
Jose Carlos Esteban <jc.esteban.gomez@gmail.com>
2017-07-20 21:59:39 +0200

    Diferentes cambios. Con estados en los componentes.

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,37 @@ var gulp = require("gulp"); //importamos gulp
 var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 var notify = require("gulp-notify");
+var gulpImport = require("gulp-html-import");
 
 //definir la tarea por defecto
-gulp.task("default", function(){
+gulp.task("default",["html","sass"], function(){
 
     //iniciamos el servidor de desarrollo
-    browserSync.init({server: "./src/"}); // arranca el servidor en la carpeta src
+    browserSync.init({server: "dist/"}); // arranca el servidor en la carpeta src
     
     //observa  cambios en los archivos SASS, y entones ejecuta la tarea 'sass'
     gulp.watch(["src/scss/*.scss","src/scss/**/*.scss"], ["sass"]);
 
     //observa cambios en los archivos html y recarga el navegador
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch(["src/*.html", "src/**/*.html"],["html"]);
 });
 
-//complar sass
+//compilar sass
 gulp.task("sass",function(){
     gulp.src("src/scss/style.scss") //cargamos el archivo style.scss
         .pipe(sass().on("error", function(error){ //lo compilamos con gulp-sass
             return notify().write(error); //si ocurre un error, mostramos una notificación
         })) 
-        .pipe(gulp.dest("src/css/")) //guardamos el resultado en la carpeta css
+        .pipe(gulp.dest("dist/css")) //guardamos el resultado en la carpeta css
         .pipe(browserSync.stream()) //recargue el CSS del navegador
         .pipe(notify("SASS Compilado 🤠")); //notificación 
+});
+
+//copiar e importar html
+gulp.task("html",function(){
+    gulp.src("src/*.html")
+        .pipe(gulpImport("src/components/"))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream()) //recargue el CSS del navegador
+        .pipe(notify("HTML Importado"));
 });
